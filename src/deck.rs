2083c3045f940a274ed025dee6cea09a6f1f75f2
Bert Douglas <georgehdouglas@gmail.com>
2023-04-23 03:09:44 -0500

    Very long day, lots of changes
    rewrote most of suits, ranks and cards
    using unified tables where all the info is adjacent
    using helper closure for initialization
    Wrote some methods for card
    after several go arounds with bit fields, decided to use
    pack and unpack methods and confine all the masking to these methods
    do it once neatly
    finally compiled again
    tons of warnings of unused code

--- src/deck.rs
+++ src/deck.rs
@@ -8,7 +8,6 @@ cards or decks.
 
 use once_cell::sync::Lazy;
 
-
 /*----------------------------------------------------------------------
 (c) Copyright Bert Douglas 2023.
 
@@ -33,63 +32,115 @@ Knight or Cavalier.  So far, this card is not used.
 
 #[repr(u8)]
 #[derive(Clone, Copy, Debug)]
-enum RanksEnum {
-    Z = 0,  // Reserved not used
-    A,      // Ace
-    N2,     // Two
-    N3,     // Three
-    N4,     // Four
-    N5,     // Five
-    N6,     // Six
-    N7,     // Seven
-    N8,     // Eight
-    N9,     // Nine
-    N10,    // Ten
-    J,      // Jack
-    C,      // Cavalier (aka Knight)
-    Q,      // Queen
-    K,      // King
-    F,      // Reserved not used
+enum RankCode {
+    R0 = 0,   // Reserved not used
+    Ac,       // Ace
+    N2,       // Two
+    N3,       // Three
+    N4,       // Four
+    N5,       // Five
+    N6,       // Six
+    N7,       // Seven
+    N8,       // Eight
+    N9,       // Nine
+    NT,       // Ten
+    Ja,       // Jack
+    Ca,       // Cavalier (aka Knight)
+    Qu,       // Queen
+    Ki,       // King
+    RF,       // Reserved not used
 }
 
-static RANK_NAMES:Lazy<Vec<&'static str>> = Lazy::new(|| { vec![
-    "Z",       "Ace",     "Two",     "Three",
-    "Four",    "Five",    "Six",     "Seven",
-    "Eight",   "Nine",    "Ten",     "jack",
-    "Knight",  "Queen",   "King",    "F",
-]});
+const N_RANK_CODE:usize = RankCode::RF as usize + 1;
+
+type Repr1 = [u8;1];
+type Repr2 = [u8;2];
+type Name =  [u8;10];
+
+#[derive(Clone, Copy, Debug, Default)]
+struct RankInfo {
+    index : u8,
+    code  : u8,
+    next  : u8,
+    repr1 : Repr1,
+    repr2 : Repr2,
+    name  : Name,
+}
+
+fn copy_u8_slice(src:&[u8],dst:&mut [u8]) {
+    assert_eq!(src.len(), dst.len());
+    for i in 0..dst.len() {
+        dst[i] = src[i];
+    }
+}
+
+static RANK_INFO:Lazy<[RankInfo;N_RANK_CODE]> = Lazy::new(|| {
+    use RankCode::*;
+    let ri_blank:RankInfo = Default::default();
+    let mut ari:[RankInfo;N_RANK_CODE] = [ri_blank;N_RANK_CODE];
+    let mut t = |
+        i     : usize,
+        code  : RankCode,
+        next  : RankCode,
+        repr1 : &str,
+        repr2 : &str,
+        name  : &str,
+    | {
+        ari[i].index = i as u8;
+        ari[i].code  = code as u8;
+        ari[i].next  = next as u8;
+        copy_u8_slice(repr1.as_bytes(), &mut ari[i].repr1);
+        copy_u8_slice(repr2.as_bytes(), &mut ari[i].repr2);
+        copy_u8_slice(name.as_bytes(),  &mut ari[i].name);
+    };
+    //  index    code   next   repr1    repr2    name
+    t(   0x0,     R0,    R0,    "Z",     "R0",   "Reserved_0" );
+    t(   0x1,     Ac,    N2,    "A",     "Ac",   "Ace"        );
+    t(   0x2,     N2,    N3,    "2",     "B2",   "Two"        );
+    t(   0x3,     N3,    N4,    "3",     "N3",   "Three"      );
+    t(   0x4,     N4,    N5,    "4",     "N4",   "Four"       );
+    t(   0x5,     N5,    N6,    "5",     "N5",   "Five"       );
+    t(   0x6,     N6,    N7,    "6",     "N5",   "Six"        );
+    t(   0x7,     N7,    N8,    "7",     "N7",   "Seven"      );
+    t(   0x8,     N8,    N9,    "8",     "N8",   "Eight"      );
+    t(   0x9,     N9,    NT,    "9",     "N9",   "Nine"       );
+    t(   0xA,     NT,    Ja,    "T",     "NT",   "Ten"        );
+    t(   0xB,     Ja,    Qu,    "J",     "Ja",   "Jack"       );
+    t(   0xC,     Ca,    R0,    "C",     "Ca",   "Knight"     );
+    t(   0xD,     Qu,    Ki,    "Q",     "Qu",   "Queen"      );
+    t(   0xE,     Ki,    R0,    "K",     "Ki",   "King"       );
+    t(   0xF,     RF,    R0,    "F",     "rF",   "Reserved_F" );
+    ari
+});
 
 #[test]
 fn test_ranks() {
-    use RanksEnum::*;
-    assert_eq!(0x0, Z   as u8);
-    assert_eq!(0x1, A   as u8);
-    assert_eq!(0x4, N4  as u8);
-    assert_eq!(0x7, N7  as u8);
-    assert_eq!(0xA, N10 as u8);
-    assert_eq!(0xB, J   as u8);
-    assert_eq!(0xC, C   as u8);
-    assert_eq!(0xD, Q   as u8);
-    assert_eq!(0xE, K   as u8);
-    assert_eq!(0xF, F   as u8);
-
-    assert_eq!("Ace",    RANK_NAMES[A  as usize] );
-    assert_eq!("Two",    RANK_NAMES[N2 as usize] );
-    assert_eq!("Five",   RANK_NAMES[N5 as usize] );
-    assert_eq!("Nine",   RANK_NAMES[N9 as usize] );
-    assert_eq!("jack",   RANK_NAMES[J  as usize] );
-    assert_eq!("Knight", RANK_NAMES[C  as usize] );
-    assert_eq!("Queen",  RANK_NAMES[Q  as usize] );
-    assert_eq!("King",   RANK_NAMES[K  as usize] );
+    let ri = &RANK_INFO;
+    for i in 0..ri.len() {
+        assert_eq!(i, ri[i].index as usize);
+        assert_eq!(i, ri[i].code as usize);
+        assert_eq!(format!("{:?}",ri[i].code), ri[i].repr2);
+        // FIXME "next" field not tested
+    }
 }
 
-static RANKS:Lazy<Vec<u8>> = Lazy::new(|| {
-    use RanksEnum::*;
-    vec![
-        A as u8,   N2 as u8,  N3 as u8,  N4 as u8,  N5 as u8,
-        N6 as u8,  N7 as u8,  N8 as u8,  N9 as u8,  N10 as u8,
-        J as u8,   Q as u8,   K as u8,
-    ]
+type Rank = u8;
+const N_RANKS:usize = 13;
+
+// ranks in standard deck in order
+static RANKS:Lazy<[Rank;N_RANKS]> = Lazy::new(|| {
+    use RankCode::*;
+    let vr:Vec<RankCode> = vec![
+        Ac,N2,N3,N4,N5,
+        N6,N7,N8,N9,NT,
+        Ja,Qu,Ki,
+    ];
+    let mut ar:[Rank;N_RANKS] = [0;N_RANKS];
+    assert_eq!(ar.len(), vr.len());
+    for i in 0..ar.len() {
+        ar[i] = vr[i] as Rank;
+    }
+    ar
 });
 
 /*----------------------------------------------------------------------
@@ -115,62 +166,95 @@ diamond   0b01      0         1       0xC0
 club      0b10      1         0       0xD0
 heart     0b11      1         1       0xB0
 =======   ====     =====     ===     =======
+
+
+Unicode to SuitCode translation table
+
+========   =======   =======
+SuitCode   unicode   unicode
+           [7..4]    [5..4]
+========   =======   =======
+   1         0xC      0b00
+   2         0xD      0b01
+   0         0xA      0b10
+   3         0xB      0b11
+========   =======   =======
+
 */
 
 // bit masks to extract suit attributes
-pub static SUIT_RED:u8   = 0x10;
-pub static SUIT_ROUND:u8 = 0x20;
-
-// used to convert suit code to unicode
-static SUIT_TO_UNICODE_LUT:Lazy<Vec<u32>> = Lazy::new(|| {
-    vec![0xA0,0xC0,0xD0,0xB0]
-});
+static SUIT_RED:u8        = 0x10;
+static SUIT_COLOR_MASK:u8 = 0x10;
+static SUIT_ROUND:u8      = 0x20;
+static SUIT_SHAPE_MASK:u8 = 0x20;
 
 #[repr(u8)]
 #[derive(Clone, Copy, Debug)]
-enum SuitsEnum {
-    Spa = 0,     // spade
-    Dia,         // diamond
-    Clu,         // club
-    Hea,         // heart
+enum SuitCode {
+    Sp = 0,     // spade
+    Di,         // diamond
+    Cl,         // club
+    He,         // heart
 }
 
-static SUIT_NAMES:Lazy<Vec<&'static str>> = Lazy::new(|| {
-    vec![
-        "Spades",
-        "Diamonds",
-        "Clubs",
-        "Hearts",
-    ]
+enum SuitColor {
+    Red = 0,
+    Black,
+}
+
+const N_SUIT_CODE:usize = SuitCode::He as usize + 1;
+
+#[derive(Clone, Copy, Debug, Default)]
+struct SuitInfo {
+    index    : u8,
+    code     : u8,
+    color    : u8,
+    to_uni   : u8,
+    from_uni : u8,
+    name     : [u8;8],
+}
+
+static SUIT_INFO:Lazy<[SuitInfo;N_SUIT_CODE]> = Lazy::new(|| {
+    use SuitCode::*;
+    use SuitColor::*;
+    let si_blank:SuitInfo = Default::default();
+    let mut asi:[SuitInfo;N_SUIT_CODE] = [si_blank;N_SUIT_CODE];
+    let mut t = |
+        i        : usize,
+        code     : SuitCode,
+        color    : SuitColor,
+        to_uni   : u8,
+        from_uni : SuitCode,
+        name     : &str,
+    | {
+        asi[i].index    = i as u8;
+        asi[i].code     = code as u8;
+        asi[i].color    = color as u8;
+        asi[i].to_uni   = to_uni;
+        asi[i].from_uni = from_uni as u8;
+        copy_u8_slice(name.as_bytes(), &mut asi[i].name);
+    };
+    //                            to       from
+    //  index   code   color    unicode   unicode   name
+    t(    0,     Sp,   Black,    0xA0,       Di,    "Spades",   );
+    t(    1,     Di,   Red,      0xC0,       Cl,    "Diamonds", );
+    t(    2,     Cl,   Black,    0xD0,       Sp,    "Clubs",    );
+    t(    3,     He,   Red,      0xB0,       He,    "Hearts",   );
+    asi
 });
 
-static SUITS:Lazy<Vec<u8>> = Lazy::new(|| {
-    use SuitsEnum::*;
-    vec![Spa as u8, Dia as u8, Clu as u8, Hea as u8,]
+static SUITS:Lazy<[u8;N_SUIT_CODE]> = Lazy::new(|| {
+    let info = &SUIT_INFO;
+    let mut suits:[u8;N_SUIT_CODE] = [0;N_SUIT_CODE];
+    for i in 0..N_SUIT_CODE {
+        suits[i] = info[i].code;
+    }
+    suits
 });
 
 #[test]
 fn test_suits() {
-    use SuitsEnum::*;
-    assert_eq!(0x0, Spa  as u8);
-    assert_eq!(0x1, Dia  as u8);
-    assert_eq!(0x2, Clu  as u8);
-    assert_eq!(0x3, Hea  as u8);
-
-    assert!((Spa & SUIT_RED) == 0);
-    assert!((Dia & SUIT_RED) != 0);
-    assert!((Clu & SUIT_RED) == 0);
-    assert!((Hea & SUIT_RED) != 0);
-
-    assert!((Spa & SUIT_ROUND) == 0);
-    assert!((Dia & SUIT_ROUND) == 0);
-    assert!((Clu & SUIT_ROUND) != 0);
-    assert!((Hea & SUIT_ROUND) != 0);
-
-    assert_eq!("Spades",   SUIT_NAMES[Spa as usize] );
-    assert_eq!("Diamonds", SUIT_NAMES[Dia as usize] );
-    assert_eq!("Clubs",    SUIT_NAMES[Clu as usize] );
-    assert_eq!("Hearts",   SUIT_NAMES[Hea as usize] );
+    // FIXME
 }
 
 /*-----------------------------------------------------------------------
@@ -184,42 +268,113 @@ FaceUp is always set to zero for making and shuffling the deck.
 
 Bit 7 must be zero for card bytes.
 
-+--------+--------+--------+--------+--------+--------+--------+--------+
-|   7    |   6    |   5    |   4    |   3    |   2    |   1    |   0    |
-+--------+--------+--------+--------+--------+--------+--------+--------+
-|   0    | FaceUp |       Suit      |                Rank               |
-+--------+--------+--------+--------+--------+--------+--------+--------+
++---------+--------+--------+--------+--------+--------+--------+--------+
+|   7     |   6    |   5    |   4    |   3    |   2    |   1    |   0    |
++---------+--------+--------+--------+--------+--------+--------+--------+
+| Group=0 | FaceUp |       Suit      |                Rank               |
++---------+--------+--------+--------+--------+--------+--------+--------+
 
 */
 
-fn card_to_unicode(card: u8) -> char {
-    let suit:usize = ((card>>4) & 0x3).into();
-    let rank:u32 = (card & 0x0F).into();
-    let u:u32 = 0x1f000 | SUIT_TO_UNICODE_LUT[suit] | rank;
-    char::from_u32(u).unwrap()
+static CARD_UNICODE_BASE:u32 = 0x1F000;
+
+pub struct Card {
+    pub code: u8,
 }
 
-fn valid_card(card:u8) -> bool {
-    let r = card & 0xf;
-    let g = card & 0x80;
-    let mut valid:bool = true;
-    // FIXME not handling the Cavalier card
-    valid &=  g == 0;
-    valid &=  r != 0x0;
-    valid &=  r != 0xf;
-    valid
+#[derive(Clone, Copy, Debug, Default)]
+struct UnpackedCard {
+    group   : bool,
+    face_up : bool,
+    suit    : u8,
+    rank    : u8,
 }
+
+impl Card {
+fn unpack(&self) -> UnpackedCard {
+    UnpackedCard {
+        group   : ( self.code & 0b1_0_00_0000) != 0,
+        face_up : ( self.code & 0b0_1_00_0000) != 0,
+        suit    : ((self.code & 0b0_0_11_0000) >> 4),
+        rank    : ((self.code & 0b0_0_00_1111) >> 0),
+    }
+}}
+
+impl Card {
+fn pack(uc:&UnpackedCard) -> Card {
+    Card { code :
+        0
+        |  ((uc.group   as u8) << 7)
+        |  ((uc.face_up as u8) << 6)
+        |  ((uc.suit    as u8) << 4)
+        |  ((uc.rank    as u8) << 0)
+    }
+}}
+
+impl Card {
+fn to_unicode(&self) -> char {
+    let up = self.unpack();
+    let u:u32 = 0
+        |   CARD_UNICODE_BASE
+        |   (SUIT_INFO[up.suit as usize].to_uni as u32)
+        |   (up.rank as u32);
+    char::from_u32(u).unwrap()
+}}
+
+impl Card {
+fn from_unicode(c:char) -> Card {
+    // unpack the unicode
+    let u = c as u32;
+    let     base:u32 = (u & 0xFFFFFF00)  as u32;
+    let mut suit:u8  = (u & 0x000000F0)  as u8;
+    let     rank:u8  = (u & 0x0000000F)  as u8;
+    // detect bad values
+    assert_eq!(base,CARD_UNICODE_BASE);
+    assert!(suit >= 0xA0 && suit <= 0xD0);
+    // translate suit to internal code
+    suit = (suit >> 4) & 0x3;
+    suit = SUIT_INFO[suit as usize].from_uni;
+    // construct unpacked card
+    Card::pack ( &UnpackedCard {
+        group   : false,
+        face_up : false,
+        suit    : suit,
+        rank    : rank,
+    })
+}}
+
+impl Card {
+fn valid(&self) -> bool {
+    let up = self.unpack();
+    true
+    & !up.group
+    & RANKS.iter().any(|&r| r == up.rank)
+}}
+
+impl Card {
+// two cards have the same color
+pub fn same_color(&self,other:&Card) -> bool {
+    0 == ((self.code ^ other.code) & SUIT_COLOR_MASK)
+}}
+
+impl Card {
+// other is next ascending rank to self
+pub fn rank_next(&self,other:Card) -> bool {
+    let rs:u8 = self.code & 0xf;
+    let ro:u8 = other.code & 0xf;
+    ro == RANK_INFO[rs as usize].next
+}}
 
 /*----------------------------------------------------------------------
 Deck object
 */
 
-pub const NDECK:usize = 52;
+const NDECK:usize = 52;
 
 #[derive(Clone, Debug)]
 pub struct Deck {
     pub kind:Kind,
-    pub cards:Vec<u8>,
+    pub cards:[u8;NDECK],
 }
 
 #[repr(u8)]
@@ -227,22 +382,21 @@ pub struct Deck {
 pub enum Kind {
     Blank,         // not yet initialized
     Selectors,     // random bits used in shuffling
-    Ordinals,      // sequential 0..N
-    BitCodes,      // card codes with bit flags for fast classification
+    Canonical,     // card codes with bit flags for fast classification
     Unicode,       // unicode for display
 }
 
 pub static BLANK:Lazy<Deck> = Lazy::new(|| {
     Deck {
         kind  : Kind::Blank,
-        cards : vec![0;NDECK],
+        cards : [0;NDECK],
     }
 });
 
 // convenient card codes
-pub static BIT_CODES:Lazy<Deck> = Lazy::new(|| {
+pub static CANONICAL:Lazy<Deck> = Lazy::new(|| {
     let mut out:Deck = (*BLANK).clone();
-    out.kind = Kind::BitCodes;
+    out.kind = Kind::Canonical;
     let mut i = 0;
     assert_eq!(out.cards.len(), (*SUITS).len() * (*RANKS).len());
     for s in (*SUITS).clone() {
@@ -250,16 +404,6 @@ pub static BIT_CODES:Lazy<Deck> = Lazy::new(|| {
             out.cards[i] = (s<<4) | r;
             i += 1;
         }
-    }
-    out
-});
-
-// sequence of numbers from 0 to 51
-pub static ORDINALS:Lazy<Deck> = Lazy::new(|| {
-    let mut out:Deck = (*BLANK).clone();
-    out.kind = Kind::Ordinals;
-    for i in 0..out.cards.len() {
-        out.cards[i] = i as u8;
     }
     out
 });
@@ -689,6 +833,5 @@ A run is a sequence of values that increase or decrease
 //}
 
 */
-
 
 // end mod deck
